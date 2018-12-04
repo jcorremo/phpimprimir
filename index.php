65dd21bb612a667d9f0598967fc4a0d6959101f2
jcorremo <jcorremo1998@gmail.com>
2018-12-04 08:39:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "micro-vertex/clientes");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$res = curl_exec($ch);
+	curl_close($ch);
+	echo $res;
+?>
