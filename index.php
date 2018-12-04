663579c7b7cb9126959961d74c0db1702c847592
jcorremo <jcorremo1998@gmail.com>
2018-12-04 08:45:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-
+	echo 'ALGO';
+	/*
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "micro-vertex/clientes");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$res = curl_exec($ch);
 	curl_close($ch);
 	echo $res;
+	*/
 ?>
